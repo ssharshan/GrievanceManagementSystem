065471d262dbfa359305a42eeba1fce3ce9acbf2
sriharsha-skavatar <98428058+sriharsha-skavatar@users.noreply.github.com>
2023-10-24 02:45:27 +0530

    OpenAPI Contract Base commit

---
+++ src/main/java/com/scaler/greivance/GreivanceApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.greivance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreivanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreivanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/greivance/GreivanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.greivance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreivanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
